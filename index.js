6c819686702e994d04c16b9ec268620a701b72ac
Ronnie Nygren <r.k.n.post@gmail.com>
2020-03-13 13:27:07 +0200

    Create async function for await calls

--- index.js
+++ index.js
@@ -3,35 +3,38 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const SentryCli = require('@sentry/cli');
 
-try {
-  const sentryOrg = core.getInput('sentry-organisation');
-  const sentryProj = core.getInput('sentry-project');
-  const sentryAuthKey = core.getInput('sentry-auth-key');
-  const confFile = `
-    [auth]
-    token=${sentryAuthKey}
-    [defaults]
-    project = ${sentryProj}
-    org = ${sentryOrg}
-  `;
-  fs.writeFileSync('.sentryclirc', confFile, {
-      flag: 'w+'
-  })
-  const releaseId = core.getInput('release-id');
-  const sourceMapLoc = core.getInput('source-map-location');
-  const sourceMapPre = core.getInput('source-map-prefix');
-
-  const cli = new SentryCli('.sentryclirc');
-  const release = releaseId || cli.releases.proposeVersion();
-  console.log(`Release ID is ${release}`);
-  await cli.releases.new(release);
-  await cli.releases.uploadSourceMaps(release, {
-      include: [sourceMapLoc],
-      urlPrefix: sourceMapPre || undefined
-  });
-  await cli.releases.finalize(release);
-  core.setOutput("versionId", version);
-  fs.unlinkSync('.sentryclirc');
-} catch (error) {
-  core.setFailed(error.message);
-}
+async function run() {
+    try {
+        const sentryOrg = core.getInput('sentry-organisation');
+        const sentryProj = core.getInput('sentry-project');
+        const sentryAuthKey = core.getInput('sentry-auth-key');
+        const confFile = `
+          [auth]
+          token=${sentryAuthKey}
+          [defaults]
+          project = ${sentryProj}
+          org = ${sentryOrg}
+        `;
+        fs.writeFileSync('.sentryclirc', confFile, {
+            flag: 'w+'
+        })
+        const releaseId = core.getInput('release-id');
+        const sourceMapLoc = core.getInput('source-map-location');
+        const sourceMapPre = core.getInput('source-map-prefix');
+      
+        const cli = new SentryCli('.sentryclirc');
+        const release = releaseId || cli.releases.proposeVersion();
+        console.log(`Release ID is ${release}`);
+        await cli.releases.new(release);
+        await cli.releases.uploadSourceMaps(release, {
+            include: [sourceMapLoc],
+            urlPrefix: sourceMapPre || undefined
+        });
+        await cli.releases.finalize(release);
+        core.setOutput("versionId", version);
+        fs.unlinkSync('.sentryclirc');
+      } catch (error) {
+        core.setFailed(error.message);
+      }
+} 
+run();
