0cbc83e1bcd48b62afdf9d0b294bc6f7e90d6f7d
Juha Suni <juha.suni@showell.com>
2022-11-10 10:57:15 +0200

    Try fixing Sentry deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -l
 
-# $1 = sentry-organisation
-# $2 = sentry-project
-# $3 = sentry-auth-key
-# $4 = release-id
-# $5 = source-map-loction
-# $6 = source-map-prefix
 set -e
 
 # Respect RELEASE_VERSION if specified
@@ -14,11 +8,11 @@ set -e
 export SENTRY_AUTH_TOKEN=$INPUT_SENTRYAUTHTOKEN
 export SENTRY_LOG_LEVEL=info
 
+# Capture output
+output=$(
 echo "Release version is ${RELEASE_VERSION}"
 echo "Organization is ${INPUT_SENTRYORGANIZATION}"
 echo "Project is ${INPUT_SENTRYPROJECT}"
-# Capture output
-output=$(
 sentry-cli releases -o ${INPUT_SENTRYORGANIZATION} -p ${INPUT_SENTRYPROJECT} new ${RELEASE_VERSION}
 sentry-cli releases -o ${INPUT_SENTRYORGANIZATION} -p ${INPUT_SENTRYPROJECT} set-commits --auto ${RELEASE_VERSION}
 sentry-cli releases -o ${INPUT_SENTRYORGANIZATION} -p ${INPUT_SENTRYPROJECT} files ${RELEASE_VERSION} upload-sourcemaps ${INPUT_SOURCEMAPLOCATION}
@@ -28,7 +22,5 @@ sentry-cli releases -o ${INPUT_SENTRYORGANIZATION} -p ${INPUT_SENTRYPROJECT} fin
 # Preserve output for consumption by downstream actions
 echo "$output" > "${HOME}/${GITHUB_ACTION}.${log}"
 
-
 # Write output to STDOUT
 echo "$output"
-
